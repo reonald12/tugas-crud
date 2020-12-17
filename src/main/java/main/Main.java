3faf2543cd6a66d63f89e68aa9d28da1cc55eba3
reonald12 <76093428+reonald12@users.noreply.github.com>
2020-12-17 15:52:12 +0800

    Add files via upload

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import model.Mahasiswa;
+
+public class Main {
+    public static void main(String[] args) {
+        Mahasiswa mahasiswa = new Mahasiswa("1", "popopopo", 3.14, 10);
+        mahasiswa.create();
+        
+    }
+}
